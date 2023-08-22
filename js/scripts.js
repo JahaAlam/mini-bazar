46a9cd32d056db91c82ccab04685a52882b3bbc2
JahangirAlam <jahangiralamarif34@gmail.com>
2023-08-22 15:05:19 +0200

    total price calculation done

--- js/scripts.js
+++ js/scripts.js
@@ -11,47 +11,90 @@
      const showItemsElement = showItems.innerText; 
    /* // const showItemList = document.createElement('li');
     showItemList.innerText = price; */
- 
+let total = 0;
 
-//----------------------item
-function itemsNameAndPrice(){
-    const price =  getElementPrice('item-price');
-    console.log(price);
+//----------------------item-1
+function itemsNameAndPrice() {
+    const price = getElementPrice('item-price');
+    //console.log(typeof (price));
     const itemName = getItemName('item-name');
-    console.log(itemName);
+    // console.log(itemName);
+    itemNameList(itemName);
+//calculate total
+    total = parseFloat(total) + parseFloat(price);
+    console.log((total).toFixed(2));
+
+    //set the price
+    setItemPrice('total-price', total);
+
 }
 
-//--------------item 2
-function itemsNameAndPrice2(){
-    const price =  getElementPrice('item2-price');
-    console.log(price);
+//--------------item 2--------------------------------
+function itemsNameAndPrice2() {
+    const price = getElementPrice('item2-price');
+    //console.log(price);
     const itemName = getItemName('item2-name');
-    console.log(itemName);
+    //console.log(itemName);
+    //setItemName('show-item2', itemName);
+    itemNameList(itemName);
+//calculate total
+    total = parseFloat(total) + parseFloat(price);
+    console.log((total).toFixed(2));
+     //set the price
+     setItemPrice('total-price', total);
+
 }
-function itemsNameAndPrice3(){
-    const price =  getElementPrice('item3-price');
-    console.log(price);
+
+function itemsNameAndPrice3() {
+    const price = getElementPrice('item3-price');
+    //console.log(price);
     const itemName = getItemName('item3-name');
-    console.log(itemName);
+   // console.log(itemName);
+    itemNameList(itemName);
+//calculate total
+    total = parseFloat(total) + parseFloat(price);
+    console.log((total).toFixed(2));
+     //set the price
+     setItemPrice('total-price', total);
 }
-function itemsNameAndPrice4(){
-    const price =  getElementPrice('item4-price');
-    console.log(price);
+function itemsNameAndPrice4() {
+    const price = getElementPrice('item4-price');
+    //console.log(price);
     const itemName = getItemName('item4-name');
-    console.log(itemName);
+    //console.log(itemName);
+    itemNameList(itemName);
+    //calculate total
+    total = parseFloat(total) + parseFloat(price);
+    console.log((total).toFixed(2));
+     //set the price
+     setItemPrice('total-price', total);
 }
-function itemsNameAndPrice5(){
-    const price =  getElementPrice('item5-price');
-    console.log(price);
+function itemsNameAndPrice5() {
+    const price = getElementPrice('item5-price');
+   // console.log(price);
     const itemName = getItemName('item5-name');
-    console.log(itemName);
+    //console.log(itemName);
+    itemNameList(itemName);
+    //calculate total
+    total = parseFloat(total) + parseFloat(price);
+    console.log((total).toFixed(2));
+     //set the price
+     setItemPrice('total-price', total);
 }
-function itemsNameAndPrice6(){
-    const price =  getElementPrice('item6-price');
-    console.log(price);
+function itemsNameAndPrice6() {
+    const price = getElementPrice('item6-price');
+    //console.log(price);
     const itemName = getItemName('item6-name');
-    console.log(itemName);
+    //console.log(itemName);
+    itemNameList(itemName);
+    //calculate total
+    total = parseFloat(total) + parseFloat(price);
+    console.log((total).toFixed(2));
+     //set the price
+     setItemPrice('total-price', total);
 }
+
+
 
 /* function ItemName(){
     const name = getItemName('item-name');
@@ -59,19 +102,41 @@ function itemsNameAndPrice6(){
     
 } */
 
-function getItemName(elmentNameId){
+function getItemName(elmentNameId) {
     const itemName = document.getElementById(elmentNameId);
     const itemNameText = itemName.innerText;
     return itemNameText;
 }
 
-function getElementPrice(elementId){
+function getElementPrice(elementId) {
     const itemPrice = document.getElementById(elementId);
     const itemPriceValueString = itemPrice.innerText;
-    const price = parseFloat(itemPriceValueString).toFixed(2);
+    const price = parseFloat(itemPriceValueString);
     return price;
-   /*  console.log(price); */
+    /*  console.log(price); */
 }
+
+//set item price
+function setItemPrice(itemPriceId, total) {
+    //console.log('working');
+    const itemPrice = document.getElementById(itemPriceId);
+    itemPrice.innerText = total;
+
+}
+
+function itemNameList(itemName) {
+    //console.log('testing count')
+    const itemList = document.getElementById('item-list');
+    const count = itemList.childElementCount;
+    const p = document.createElement('p');
+    p.innerHTML = `${count + 1}. ${itemName}`;
+    itemList.appendChild(p);
+
+}
+
+
+
+
 
 
 
